ef57b2afb034b31f4c8c0a2cc4e526fef3b96a86
Alexandra <amckoy@purdue.edu>
2026-03-24 17:45:33 -0400

    fixed bug

--- testing.py
+++ testing.py
@@ -13,7 +13,7 @@ vehicle = { #i am making these all up fyi
     "lf" : 1.8,
     "lr" : 1.2,
     "m" : 1000,
-    "psi" : 30,
+    "delta" : 30,
     "u" : 40,
     "v" : 5,
     "r" : 10,
@@ -22,9 +22,9 @@ vehicle = { #i am making these all up fyi
 
 def ddx_longvelocity(f_xr, f_xf, f_yr, f_yf, fxother): # 5 arugments
     #dyanmic single track, outputs udot, vdot, rdot
-    udot = (1/vehicle["m"]) * (f_xr - f_yf * (np.sin(vehicle["psi"])) + f_xf*np.cos(vehicle["psi"]) + fxother) + (vehicle["v"] * vehicle["r"])
-    vdot = (1/vehicle["m"]) * (f_yr + f_yf * (np.cos(vehicle["psi"])) + f_xf*np.sin(vehicle["psi"])) - (vehicle["u"] * vehicle["r"])
-    rdot = (1/vehicle["iz"]) * (f_yf * vehicle["lf"] * (np.cos(vehicle["psi"])) + f_xf * vehicle["lf"] * np.sin(vehicle["psi"]) - (f_yf * vehicle["lr"]))
+    udot = (1/vehicle["m"]) * (f_xr - f_yf * (np.sin(vehicle["delta"])) + f_xf*np.cos(vehicle["delta"]) + fxother) + (vehicle["v"] * vehicle["r"])
+    vdot = (1/vehicle["m"]) * (f_yr + f_yf * (np.cos(vehicle["delta"])) + f_xf*np.sin(vehicle["delta"])) - (vehicle["u"] * vehicle["r"])
+    rdot = (1/vehicle["iz"]) * (f_yf * vehicle["lf"] * (np.cos(vehicle["delta"])) + f_xf * vehicle["lf"] * np.sin(vehicle["delta"]) - (f_yf * vehicle["lr"]))
     return udot, vdot, rdot
 
 result = ddx_longvelocity(500,300,400,200,50)
