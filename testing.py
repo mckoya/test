a290fbb64949835692806ada690002e449dc402f
Alexandra <amckoy@purdue.edu>
2026-03-24 17:32:58 -0400

    i wrote some code this time fr

--- testing.py
+++ testing.py
@@ -3,3 +3,24 @@
 #lets do this thing
 
 print("hello world")
+
+#okaaaay lets go
+
+import numpy as np
+
+vehicle = { #i am making these all up fyi
+    "l" : 3,
+    "lf" : 1.8,
+    "lr" : 1.2,
+    "m" : 1000,
+    "psi" : 30,
+    "u" : 40,
+    "v" : 5,
+    "r" : 10,
+}
+
+def ddx_longvelocity(f_xr, f_xf, f_yr, f_yf, fxother) :
+    #dyanmic single track, outputs udot, vdot, rdot
+    udot = (1/vehicle["m"]) * (f_xr - f_yf * (np.sin(vehicle["psi"])) + f_xf*np.cos(vehicle["psi"]) + fxother) + (vehicle["v"] * vehicle["r"])
+    vdot = (1/vehicle["m"]) * (f_yr + f_yf * (np.cos(vehicle["psi"])) + f_xf*np.sin(vehicle["psi"])) - (vehicle["u"] * vehicle["r"])
+    rdot = 
