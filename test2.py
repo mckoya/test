9d47217315836df7c92238f4eeb1d3fe71c953b2
Alexandra <amckoy@purdue.edu>
2026-03-24 18:23:23 -0400

    bruh

--- test2.py
+++ test2.py
@@ -8,6 +8,7 @@ import numpy as np
 #vehicle parameters
 vehicle = {
     "u" : 30,
+    "v" : 10,
     "delta" : np.radians(30),
     "psi" : np.radians(15),
     "l" : 3,
@@ -17,4 +18,14 @@ vehicle = {
 
 #beta. sideslip angle
 beta = np.arctan((vehicle["lr"]/vehicle["l"]) * np.tan(vehicle["delta"]))
-print(beta)
+
+#declare starting shit
+X = 0
+Y = 0
+psi = 0
+dt = 0.05
+
+#these are the derivatives used in the euler new = old + derivative * dt
+Xdot = vehicle["u"] * np.cos(vehicle["psi"] + vehicle["delta"])
+Ydot = vehicle["v"] * np.sin(vehicle["psi"] + vehicle["delta"])
+psidot = (vehicle["u"]/vehicle["lr"]) * np.sin(beta)
