597bdbb70d169e281e520e722bd171607d984ed8
Alexandra <amckoy@purdue.edu>
2026-03-25 12:50:14 -0400

    hi

--- test3.py
+++ test3.py
@@ -8,7 +8,9 @@ def lat_tire_f(c_alpha, alpha):
     fyl = -2 * c_alpha * alpha 
     return fyl
 
-c_alpha = int(input("bruh moment 1?"))
-alpha = int(input("brhu m oment 2?"))
+c_alpha = float(input("bruh moment 1?"))
+alpha = float(input("brhu m oment 2?"))
 answer = lat_tire_f(c_alpha, alpha)
 print(answer)
+
+#why is git being annoying
